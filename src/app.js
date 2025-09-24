2a30767b7817cef6a7ea1b7a587962a9f0fd1df6
Juan Camilo Zuñiga Potes <juanczuniga@estudiante.uniajc.edu.co>
2025-09-24 15:03:19 -0500

    explicacion

--- src/app.js
+++ src/app.js
@@ -986,7 +986,7 @@ app.get('/test-di', (req, res) => {
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Liga Inyección API v1.1.3', 
+    message: 'Liga Inyección API v1.1.4', 
     status: 'running',
     timestamp: new Date().toISOString()
   });
